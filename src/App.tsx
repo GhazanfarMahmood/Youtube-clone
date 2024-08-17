bbf9ef460b770d3336251b3c33ddee6d9f7a2397
Ghazanfar Mahmood <ghazanfarmahmood14@gmail.com>
2024-08-17 12:45:09 +0500

    Navbar has been created

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Watch from './pages/Watch'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/search' element={<Search />} />
+      <Route path='/watch/:id' element={<Watch />}/> 
+    </Routes>
     </>
   )
 }
 
 export default App
+// in the routes at the number 3 the route watch have id in the path which means that you have to pass the id for the watch page the id will be anything after the forward slash this will be an underscore or any other thing
